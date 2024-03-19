9630689a9438e92d9048fdd66baf42d8252ceb80
André <andregparada@gmail.com>
2024-03-19 16:44:04 -0300

    Aplicando Streams no módulo HTTP

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -18,7 +18,7 @@ class OneToHundredStream extends Readable {
     }
 }
 
-fetch('http://localhost/3334', {
+fetch('http://localhost:3334', {
     method: 'POST',
     body: new OneToHundredStream(),
     duplex: 'half'
