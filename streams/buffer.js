d8c10513d0962d89e11a1746ef984d196121aff2
André <andregparada@gmail.com>
2024-03-20 18:41:00 -0300

    Entendendo Buffers no Node

---
+++ streams/buffer.js
@@ -0,0 +1,3 @@
+const buf = Buffer.from("Amou daquela vez como se fosse a última. Beijou sua melhor como se fosse a última. E cada filho seu como se fosse o único, atravessou a rua com seu passo tímido...")
+
+console.log(buf.toJSON())
